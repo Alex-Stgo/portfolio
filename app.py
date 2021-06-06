b4b2f508c5fcff47216dce11c6dc6315342f0c1d
Alex Santiago <alejandro.stg@outlook.com>
2021-06-06 15:19:20 -0500

    First load with CSS and HTML adjusted with Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import datetime as dt
+# from pymongo import MongoClient
+# from dotenv import load_dotenv
+import os
+
+# load_dotenv()
+
+def create_app():
+    app=Flask(__name__)
+    # client = MongoClient(os.environ.get("MONGODB_URI"))
+    # app.db = client.microblog
+
+    # @app.route("/", methods=["GET","POST"])
+    @app.route("/")
+    def home():
+        # print([e for e in app.db.entries.find({})])
+        # if request.method=="POST":
+        #     entry_content=request.form.get("content")
+        #     fdate=dt.datetime.today().strftime("%Y-%m-%d")
+        #     app.db.entries.insert({"content": entry_content, "date" : fdate})
+        # entries=[(entry["content"],entry["date"]) for entry in app.db.entries.find({})]
+        return render_template("index.html")
+    return app
